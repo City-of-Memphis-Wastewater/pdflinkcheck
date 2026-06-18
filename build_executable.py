1d7374c6aa53208d509dc562e9ed33e3a6ff7242
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-06-18 16:04:48 -0500

    remove --collect-all pyinstaller calls in build_executable.py for typer, click, and rich. LEave them for pdfium and pymupdf/fitz

--- build_executable.py
+++ build_executable.py
@@ -154,9 +154,9 @@ def run_pyinstaller(
         "--hidden-import", "typer",  
         "--hidden-import", "click",  
         "--hidden-import", "rich",  
-        "--collect-all", "typer",
-        "--collect-all", "click",
-        "--collect-all", "rich",
+        #"--collect-all", "typer",
+        #"--collect-all", "click",
+        #"--collect-all", "rich",
 
         #'--log-level=DEBUG',
 
