aa2f7289769c110fca1a53725af23eb21e2a1683
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-07-28 05:41:13 -0500

    first 0ass at debug toggle

--- src/pdflinkcheck/gui.py
+++ src/pdflinkcheck/gui.py
@@ -76,6 +76,8 @@ class PDFLinkCheckApp:
         self._create_widgets()
         self._initialize_menubar()
         self._apply_menu_theme()
+        self.debug_logging_enabled = False
+        self.debug_menu_index = self.tools_menu.index("end")
 
     def _initialize_vars(self):
         """Logic that takes time but doesn't need a UI yet."""
@@ -178,7 +180,10 @@ class PDFLinkCheckApp:
         self.tools_menu.add_command(label="Clear Output Window", command=self._clear_output_window)
         self.tools_menu.add_command(label="Copy Output to Clipboard", command=self._copy_output_to_clipboard)
         self.tools_menu.add_command(label="Recheck PDF Libraries", command=self._recheck_pdf_library_cache)
-
+        self.tools_menu.add_command(
+            label="Turn Debug Logging On",
+            command=self._toggle_debug_logging
+        )
         self.tools_menu.add_separator()
         self.tools_menu.add_command(label="License", command=self._show_license)
         self.tools_menu.add_command(label="Readme", command=self._show_readme)
@@ -191,6 +196,22 @@ class PDFLinkCheckApp:
             level=level,
             format="%(asctime)s %(levelname)s %(name)s: %(message)s"
         )
+    def _toggle_debug_logging(self):
+        self.debug_logging_enabled = not self.debug_logging_enabled
+
+        if self.debug_logging_enabled:
+            logging.getLogger("pdflinkcheck").setLevel(logging.DEBUG)
+            label = "Turn Debug Logging Off"
+        else:
+            logging.getLogger("pdflinkcheck").setLevel(logging.INFO)
+            label = "Turn Debug Logging On"
+
+        self.tools_menu.entryconfig(
+            self.debug_menu_index,
+            label=label
+        )
+
+        logger.debug("Debug logging enabled")
         
     # --- UI Component Building ---
 
